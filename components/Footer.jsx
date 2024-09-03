9bbc686da37ac906ed3bf15e474d1b77aab9ed5f
Manpreet-s1ngh <singh.preet98721@gmail.com>
2024-09-03 00:29:52 -0400

    first commit

---
+++ components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import {AiFillInstagram, AiOutlineTwitter} from 'react-icons/ai'
+
+function Footer() {
+  return (
+    <div className='footer-container'>
+    <p> 2022 JSM Headphones All rights reserved </p>
+      <p className='icons'>
+      <AiFillInstagram/>
+      <AiOutlineTwitter/></p>
+    </div>
+  )
+}
+
+export default Footer
+
+/* 5:56:58 Here is the time when I Stopped watching tutorial , then tommarow I wikll continue form the same timeline*/  
---
+++ sanity_ecommerce/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'hmj46j99',
+    dataset: 'production'
+  }
+})
